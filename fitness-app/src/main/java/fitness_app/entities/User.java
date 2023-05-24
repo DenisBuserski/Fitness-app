07cffb6e15db9da8716fe6a95a9947170442215e
Denis Buserski <denis.buserski@gmail.com>
2023-05-24 15:43:55 +0300

    Create User class + fields

---
+++ fitness-app/src/main/java/fitness_app/entities/User.java
@@ -0,0 +1,13 @@
+package fitness_app.entities;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private int age;
+    
+
+}
