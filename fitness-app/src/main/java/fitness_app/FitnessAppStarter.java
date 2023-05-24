d1f219428a633f60c956c3588d0f1c00c5d160bc
Denis Buserski <denis.buserski@gmail.com>
2023-05-24 23:08:18 +0300

    Add annotation to FitnessAppStarter

--- fitness-app/src/main/java/fitness_app/FitnessAppStarter.java
+++ fitness-app/src/main/java/fitness_app/FitnessAppStarter.java
@@ -1,7 +1,11 @@
 package fitness_app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class FitnessAppStarter {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(FitnessAppStarter.class, args);
     }
 }
