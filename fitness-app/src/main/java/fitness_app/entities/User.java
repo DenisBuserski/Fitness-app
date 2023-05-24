be8a75bf4db061d952090f98099bb88e58ca85ed
Denis Buserski <denis.buserski@gmail.com>
2023-05-24 23:14:51 +0300

    Add custom annotations to User

--- fitness-app/src/main/java/fitness_app/entities/User.java
+++ fitness-app/src/main/java/fitness_app/entities/User.java
@@ -1,5 +1,9 @@
 package fitness_app.entities;
 
+import fitness_app.annotations.Age;
+import fitness_app.annotations.Email;
+import fitness_app.annotations.Password;
+import fitness_app.annotations.Username;
 import fitness_app.enums.UserType;
 
 import javax.persistence.Entity;
@@ -12,12 +16,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Username
     private String username;
+    @Password
     private String password;
+    @Email
     private String email;
     private UserType userType;
     private String firstName;
     private String lastName;
+    @Age
     private int age;
     
 
