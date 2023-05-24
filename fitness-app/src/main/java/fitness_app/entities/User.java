7f45a3e92b0da138a7724acd6c3864fa7e995c50
Denis Buserski <denis.buserski@gmail.com>
2023-05-24 23:12:19 +0300

    Add basic annotations to User

--- fitness-app/src/main/java/fitness_app/entities/User.java
+++ fitness-app/src/main/java/fitness_app/entities/User.java
@@ -2,7 +2,15 @@ package fitness_app.entities;
 
 import fitness_app.enums.UserType;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
