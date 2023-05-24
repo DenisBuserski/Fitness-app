4615dc058e35163e7df7cb9f84a414654126d16a
Denis Buserski <denis.buserski@gmail.com>
2023-05-24 23:02:25 +0300

    Add new field to User class

--- fitness-app/src/main/java/fitness_app/entities/User.java
+++ fitness-app/src/main/java/fitness_app/entities/User.java
@@ -1,10 +1,13 @@
 package fitness_app.entities;
 
+import fitness_app.enums.UserType;
+
 public class User {
     private int id;
     private String username;
     private String password;
     private String email;
+    private UserType userType;
     private String firstName;
     private String lastName;
     private int age;
