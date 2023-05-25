cf063bc026bc488718482875ce81f8a5b1dee485
Denis Buserski <denis.buserski@gmail.com>
2023-05-25 21:51:44 +0300

    Update ConsoleRunner
    Create test user

--- fitness-app/src/main/java/fitness_app/ConsoleRunner.java
+++ fitness-app/src/main/java/fitness_app/ConsoleRunner.java
@@ -1,12 +1,22 @@
 package fitness_app;
 
+import fitness_app.enums.UserType;
+import fitness_app.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConsoleRunner implements CommandLineRunner {
+    private final UserService userService;
+
+    @Autowired
+    public ConsoleRunner(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        this.userService.createUser("test-username", "test-password", "test-email", UserType.ADMIN, "test-firstName", "test-lastName", 20);
     }
 }
