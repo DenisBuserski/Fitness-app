14ee035bf8190e562d67b3b9da0547b1ab1cc9b2
Denis Buserski <denis.buserski@gmail.com>
2023-05-25 21:48:48 +0300

    Update UserServiceImpl
    Implement method createUser

--- fitness-app/src/main/java/fitness_app/services/UserServiceImpl.java
+++ fitness-app/src/main/java/fitness_app/services/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package fitness_app.services;
 
+import fitness_app.entities.User;
 import fitness_app.enums.UserType;
 import fitness_app.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
-    
+
     @Override
     public void createUser(String username, String password, String email, UserType userType, String firstName, String lastName, int age) {
+        User user = new User(username, password, email, userType, firstName, lastName, age);
+        this.userRepository.save(user);
 
     }
 }
