2e67b7f8aba8ffc8750eea9202675bb1046c421b
Denis Buserski <denis.buserski@gmail.com>
2023-05-26 21:47:54 +0300

    Update Username validation

--- fitness-app/src/main/java/fitness_app/exception_messages/UsernameExceptions.java
+++ fitness-app/src/main/java/fitness_app/exception_messages/UsernameExceptions.java
@@ -1,7 +1,7 @@
 package fitness_app.exception_messages;
 
 public final class UsernameExceptions {
-     public static final String USERNAME_ALREADY_EXISTS = "Username length should be between 4 and 30 symbols";
+     public static final String USERNAME_ALREADY_EXISTS = "Username already exists!";
 
     private UsernameExceptions() {
     }
