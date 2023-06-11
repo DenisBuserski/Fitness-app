7f9dbb146a1161ab6c84a9904387205e1fb1d052
Denis Buserski <denis.buserski@gmail.com>
2023-06-12 01:03:23 +0300

    Update

--- fitness-app/src/main/java/fitness_app/entities/Diary.java
+++ fitness-app/src/main/java/fitness_app/entities/Diary.java
@@ -1,4 +1,14 @@
 package fitness_app.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "diaries")
 public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
 }
